e9d41a6f6377ace7bd13be9ec9a77e7579ba49bf
Yogesh Kolhe <42035456+anyogi@users.noreply.github.com>
2020-05-12 15:23:30 +0200

    Add support for node version v.x.y.z

--- install.sh
+++ install.sh
@@ -2,20 +2,6 @@
 
 set -e
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-
-INSTALLER_PATH="/usr/local/bin/node_installer"
-
-mkdir -p /usr/local/bin
-
-cat > "$INSTALLER_PATH" << 'EOF'
-#!/bin/bash
-
-set -e
-
 print_help() {
 echo 'Usage as root or with sudo:
   node_installer 8.9.1 # this installs 8.9.1 version
@@ -31,7 +17,7 @@ clean_previous_installations() {
 }
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root" 
+   echo "This script must be run as root"
    print_help
    exit 1
 fi
@@ -46,19 +32,26 @@ if [[ "$1" == 'help' ]] || [[ -z "$1" ]] ; then
   exit 0
 fi
 
-if [[ ! "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] ; then
-  echo "Please provide existing node version in 'x.y.z' format"
+if [[ ! "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] && [[ ! "$1" =~ v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Please provide existing node version in 'x.y.z' or v'x.y.z' format"
   exit 2
 fi
 
-NODE="$1"
+if [[ "$1" =~ v[0-9]+\.[0-9]+\.[0-9]+$ ]] ; then
+  echo "node version in v'x.y.z' format"
+  NODE="$1"
+elif [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]] ; then
+  echo "node version in 'x.y.z' format"
+  NODE=v"$1"
+fi
+
 TARGET=
 case $(uname) in
   "Linux")
-    TARGET="node-v"$NODE"-linux-x64"
+    TARGET="node-"$NODE"-linux-x64"
     ;;
   "Darwin")
-    TARGET="node-v"$NODE"-darwin-x64"
+    TARGET="node-"$NODE"-darwin-x64"
     ;;
    *)
     echo "System not supported"
@@ -73,9 +66,9 @@ echo "Downloading"
 export TMP_DIR=$( mktemp -d )
 cd "$TMP_DIR"
 if `which wget > /dev/null` ; then
-  wget -q https://nodejs.org/dist/v"$NODE"/"$TARGET".tar.gz
+  wget -q https://nodejs.org/dist/"$NODE"/"$TARGET".tar.gz
 elif `which curl > /dev/null` ; then
-  curl -sS -o "$TARGET".tar.gz https://nodejs.org/dist/v"$NODE"/"$TARGET".tar.gz
+  curl -sS -o "$TARGET".tar.gz https://nodejs.org/dist/"$NODE"/"$TARGET".tar.gz
 else
   echo "wget or curl command not found"
   exit 4
@@ -103,7 +96,3 @@ ln -s ../lib/node_modules/npm/bin/npm-cli.js npm
 chmod +x node npm
 rm -rf "$TMP_DIR"
 echo "Node version "$NODE" successfully installed"
-EOF
-
-chmod +x "$INSTALLER_PATH"
-echo "node_installer successfully installed"
